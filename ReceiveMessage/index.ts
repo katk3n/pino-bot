38eba1cc5222eb6d4db16b620f1b083b0bc30f9e
katk3n <kentaktwo@yahoo.com>
2022-11-14 22:24:24 +0900

    pinobot now can repeat what you say

--- ReceiveMessage/index.ts
+++ ReceiveMessage/index.ts
@@ -1,5 +1,5 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
-import { Client as LineClient } from '@line/bot-sdk';
+import { Client as LineClient, Message } from '@line/bot-sdk';
 
 const httpTrigger: AzureFunction = async (context: Context, req: HttpRequest): Promise<void> => {
   context.log('HTTP trigger function processed a request.');
@@ -8,33 +8,17 @@ const httpTrigger: AzureFunction = async (context: Context, req: HttpRequest): P
     channelSecret: process.env.LINE_SECRET,
   });
 
-  //  for (const event of req.body.events) {
-  //  }
-
-  const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = name
-    ? `Hello, ${name}. This HTTP triggered function executed successfully.`
-    : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
-
-  await client
-    .pushMessage(process.env.LINE_USER_ID, [
-      {
+  for (const event of req.body.events) {
+    if (event.type === 'message' && event.message.type === 'text') {
+      const message: Message = {
         type: 'text',
-        text: responseMessage,
-      },
-    ])
-    .catch((e: Error) => {
-      context.log(
-        JSON.stringify({
-          message: e.message,
-        })
-      );
-    });
-
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage,
-  };
+        text: `${event.message.text}にゃ`,
+      };
+      if (event.replyToken) {
+        client.replyMessage(event.replyToken, message);
+      }
+    }
+  }
 };
 
 export default httpTrigger;
